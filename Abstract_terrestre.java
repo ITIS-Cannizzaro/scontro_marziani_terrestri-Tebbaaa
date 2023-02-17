68f8309d83adb83065330dfaa5f16836a05bec4d
Tebbaaa <123165869+Tebbaaa@users.noreply.github.com>
2023-02-17 11:11:37 +0100

    Add files via upload

---
+++ Abstract_terrestre.java
@@ -0,0 +1,16 @@
+
+
+public abstract class Abstract_terrestre {
+	protected String nome;
+	protected int vita;
+	protected int velocita;
+	protected int armamento;
+
+	public Abstract_terrestre(String nome, int vita, int velocita, int armamento) {
+		this.nome="sconosciuto";
+		this.vita=50;
+		this.velocita=5;
+		this.armamento=5;
+		}
+
+}
